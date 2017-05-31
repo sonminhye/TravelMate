fea847a9432584476d17ecee0c8c7f01543b9dd6
JY <sjy910630@gmail.com>
2017-05-31 21:49:43 +0900

    로그 보이게 바꿈

--- src/main/java/com/travel/mate/dao/TravelDAO.java
+++ src/main/java/com/travel/mate/dao/TravelDAO.java
@@ -19,68 +19,57 @@ import com.travel.mate.dto.TravelRouteDTO;
 public class TravelDAO extends AbstractDAO{
 
 	public void insertTravel(TravelDTO travel) {
-		System.out.println("DAO Call : insertTravel..");
 		insert("travel.insertTravel", travel);
 	}
 
 	public void insertTravelDetail(TravelDetailDTO travelDetail) {
-		System.out.println("DAO Call : insertTravelDetail..");
 		insert("travel.insertTravelDetail", travelDetail);
 	}
 
 	public void insertTravelImage(TravelImageDTO travelImage) {
-		System.out.println("DAO Call : insertTravelImage..");
 		insert("travel.insertTravelImage", travelImage);
 	}
 
 	public void insertTravelRoute(TravelRouteDTO travelRoute) {
-		System.out.println("DAO Call : insertTravelRoute..");
 		insert("travel.insertTravelRoute", travelRoute);
 	}
 	
 	@SuppressWarnings("unchecked")
 	public List<Map<String, Object>> selectTravel(Map<String, Object> map) {
-		System.out.println("DAO Call : selectTravel..");
 		return (List<Map<String, Object>>)selectList("travel.selectTravel", map);
 	}
 
 	@SuppressWarnings("unchecked")
 	public List<Map<String, Object>> selectTravelDetail(int code) {
-		System.out.println("DAO Call : selectTravelDetail..");
 		return (List<Map<String, Object>>)selectList("travel.selectTravelDetail", code);
 	}
 
 	@SuppressWarnings("unchecked")
 	public List<Map<String, Object>> selectTravelRoute(int code) {
-		System.out.println("DAO Call : selectTravelRoute..");
 		return (List<Map<String, Object>>)selectList("travel.selectTravelRoute", code);
 	}
 
 	@SuppressWarnings("unchecked")
 	public List<Map<String, Object>> scrollDown(Integer code) {
-		System.out.println("DAO Call : scrollDown..");
 		return (List<Map<String, Object>>)selectList("travel.selectTravelScroll", code);
 	}
 
 	public void insertTravelApply(ApplyDTO apply) {
-		System.out.println("DAO Call : insertTravelApply..");
 		insert("travel.insertTravelApply", apply);
 	}
 
+	@SuppressWarnings("unchecked")
 	public List<Map<String, Object>> selectTravelApply(ApplyDTO applyDto) {
-		System.out.println("DAO Call : selectTravelApply..");
 		return (List<Map<String, Object>>)selectList("travel.selectTravelApply", applyDto);
 	}
 
+	@SuppressWarnings("unchecked")
 	public List<Map<String, Object>> selectTravelApplyCount(ApplyDTO applyDto) {
-		System.out.println("DAO Call : selectTravelApplyCount..");
 		return (List<Map<String, Object>>)selectList("travel.selectTravelApplyCount", applyDto);
 	}
 
 	public void deleteTravelApply(ApplyDTO apply) {
-		System.out.println("DAO Call : deleteTravelApply..");
 		delete("travel.deleteTravelApply", apply);
-		
 	}
 
 }
